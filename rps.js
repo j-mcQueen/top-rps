ae819d3abf1b05feb6ab5a08db1ac81a1547aac5
silverwish <jdmcqueen44@gmail.com>
2021-06-16 16:50:07 +0100

    Experimented with placement of methods on variables

--- rps.js
+++ rps.js
@@ -2,7 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 
 const game = function() {
-    for (i = 0; playerScore === 3 || computerScore === 3; i++) {
+    // for (i = 0; (playerScore === 3 || computerScore === 3); i++) {
         const computerPlay = function() {
             let choices = ["Rock", "Paper", "Scissors"];
     
@@ -13,39 +13,41 @@ const game = function() {
         const computerSelection = computerPlay();
     
         const playRound = function(playerSelection, computerSelection) {
-            if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-                console.log("Game tied! Play again.");
-                continue;
-            }
-    
-            if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+            if (playerSelection === 'rock' && computerSelection === 'scissors') {
                 playerScore += 1;
     
                 console.log(`Rock beats scissors, the score is ${playerScore}-${computerScore} to the player.`);
-            } else if (computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'scissors') {
+            } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
                 computerScore += 1;
     
                 console.log(`Rock beats scissors, the score is ${playerScore}-${computerScore} to the player.`);
             }
     
-            if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+            if (playerSelection === 'paper' && computerSelection === 'rock') {
                 playerScore += 1;
     
                 console.log(`Paper beats rock, the score is ${playerScore}-${computerScore} to player.`);
-            } else if (computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'rock') {
+            } else if (computerSelection === 'paper' && playerSelection === 'rock') {
                 computerScore += 1;
     
                 console.log(`Paper beats rock, the score is ${playerScore}-${computerScore} to the computer.`);
             }
     
-            if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
+            if (playerSelection === 'scissors' && computerSelection === 'paper') {
                 playerScore += 1;
     
                 console.log(`Scissors beats paper, the score is ${playerScore}-${computerScore} to player.`);
-            } else if (computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'paper') {
+            } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
                 computerScore += 1;
     
                 console.log(`Scissors beats paper, the score is ${playerScore}-${computerScore} to the computer.`);
+            }
+
+            if (
+                (playerSelection === 'rock' && computerSelection.toLowerCase() === 'rock') ||
+                (playerSelection === 'paper' && computerSelection.toLowerCase() === 'paper') || 
+                (playerSelection === 'scissors' && computerSelection.toLowerCase() === 'scissors')) {
+                console.log("Game tied! Play again.");
             }
     
             if (playerScore === 3) {
@@ -54,7 +56,9 @@ const game = function() {
                 console.log("Computer has reached 3 points, computer wins. Unlucky!");
             }
         }
-    }
+
+        playRound();
+    // }
 }
 
 game();
